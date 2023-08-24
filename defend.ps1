46da2b0debb61ce2f4baa27a5fb1b4409aceff2e
James Ira Arellano <105153035+AikyanP@users.noreply.github.com>
2023-08-24 21:05:35 +1000

    Update defend.ps1

--- defend.ps1
+++ defend.ps1
@@ -1,18 +1,18 @@
 # Function to configure firewall to block outbound connections to TCP port 80 (HTTP)
 function blockHTTP {
     $ruleName = "BlockOutboundHTTP"
-
+    
     # Create a new firewall rule to block outbound connections on TCP port 80
-    New-NetFirewallRule -DisplayName $ruleName -Direction Outbound -Protocol TCP -LocalPort 80 -Action Block
+    New-NetFirewallRule -Name $ruleName -Direction Outbound -Protocol TCP -LocalPort 80 -Action Block
     Write-Output "Firewall rule to block outbound HTTP traffic created."
 }
 
 # Function to test if TCP port 80 (HTTP) traffic is blocked
 function testHTTPBlock {
     $ruleName = "BlockOutboundHTTP"
-
+    
     # Check if the firewall rule exists and is enabled
-    $rule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction SilentlyContinue
+    $rule = Get-NetFirewallRule -Name $ruleName -ErrorAction SilentlyContinue
     if ($rule -and $rule.Enabled) {
         Write-Output "HTTP traffic is blocked."
     } else {
@@ -23,13 +23,17 @@ function testHTTPBlock {
 # Function to disable the firewall rule blocking HTTP traffic
 function unblockHTTP {
     $ruleName = "BlockOutboundHTTP"
-
-    # Disable and remove the firewall rule if it exists
-    $rule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction SilentlyContinue
+    
+    # Check if the firewall rule exists
+    $rule = Get-NetFirewallRule -Name $ruleName -ErrorAction SilentlyContinue
+    
     if ($rule) {
-        Set-NetFirewallRule -Name $ruleName -Enabled False
-        Remove-NetFirewallRule -Name $ruleName
-        Write-Output "Firewall rule blocking outbound HTTP traffic removed."
+        if ($rule.Enabled) {
+            Set-NetFirewallRule -Name $ruleName -Enabled False
+            Write-Output "Firewall rule blocking outbound HTTP traffic is now disabled."
+        } else {
+            Write-Output "Firewall rule blocking outbound HTTP traffic is already disabled."
+        }
     } else {
         Write-Output "No firewall rule blocking outbound HTTP traffic found."
     }
