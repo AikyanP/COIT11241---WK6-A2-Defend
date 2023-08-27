622307f41c56d45de956e4f0f99c6a7ad0cc11fd
James Ira Arellano <105153035+AikyanP@users.noreply.github.com>
2023-08-27 21:28:59 +1000

    Update Step 8.ps1

--- Step 8.ps1
+++ Step 8.ps1
@@ -2,6 +2,6 @@
 function resetDoH {
 	$interfacename = "Ethernet 3"
 	Set-DnsClientServerAddress -InterfaceAlias $interfaceName -ServerAddresses ("10.0.2.3")
-	
+
 	Write-Output "DNS server for $interfaceName reset to 10.0.2.3."
 }
